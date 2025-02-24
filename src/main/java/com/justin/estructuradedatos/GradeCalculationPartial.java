8fedbfd6933ca1bb9b474c69475420466fe49f8d
JSalim07 <96034306+JSalim07@users.noreply.github.com>
2025-02-24 10:01:01 -0600

    Examen parte 1

---
+++ src/main/java/com/justin/estructuradedatos/GradeCalculationPartial.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.justin.estructuradedatos;  
+
+/**
+ *
+ * @author justin
+ */
+class GradeCalculationPartial {
+    public double calcularCalificacionFinal(double examen, double tareas) {
+        return (examen * 0.7) + (tareas * 0.3);
+    }
+}
+
+
+
